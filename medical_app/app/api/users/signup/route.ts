53d18448866aa41e14ed5639c3b2801264120cfd
Muse-feta <fetamuse@gmail.com>
2024-07-14 18:38:10 +0300

    finished signup for backend

---
+++ medical_app/app/api/users/signup/route.ts
@@ -0,0 +1,58 @@
+import { NextResponse, NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import bcryptjs from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json();
+    // console.log("body", body);
+
+    // check if user already exists
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (user) {
+      return NextResponse.json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const salt = bcryptjs.genSaltSync(10);
+    const hashedPassword = bcryptjs.hashSync(body.password, salt);
+
+    const newUser = await prisma.$transaction(async (prisma) => {
+      const user = await prisma.user.create({
+        data: {
+          firstname: body.firstname,
+          lastname: body.lastname,
+          email: body.email,
+        },
+      });
+      const userInfo = await prisma.userInfo.create({
+        data: {
+          userId: user.id,
+          password: hashedPassword,
+          phone: body.phone,
+        },
+      });
+      return user;
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "User created successfully",
+      data: {
+        newUser,
+      },
+    });
+  } catch (error: any) {
+    console.log("error occurring in server", error.message);
+    NextResponse.json({ success: false, message: error.message });
+  }
+};
---
+++ medical_app/prisma/migrations/20240714081632_add_default_role/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `userinfo` MODIFY `role` VARCHAR(191) NOT NULL DEFAULT 'USER';
