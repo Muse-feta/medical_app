8f82a264b07fc15e998f613a8cf817cb0a60bd3a
Muse-feta <fetamuse@gmail.com>
2024-07-24 00:16:08 +0300

    manage conflicts

--- medical_app/app/layout.tsx
+++ medical_app/app/layout.tsx
@@ -4,14 +4,10 @@ import "./globals.css";
 import "@/asset/css/app.min.css";
 import "@/asset/css/style.css";
 import "@/asset/css/fontawesome.min.css";
-<<<<<<< HEAD
 // import { Footer, Header, HeaderTop } from "@/components";
 import AuthContextProvider from "@/context/authContext/AuthContextProvider";
 import Header from "@/components/Header";
 import { Footer } from "react-day-picker";
-=======
-import { Footer, Header, HeaderTop } from "@/components";
->>>>>>> parent of ac85760 (finished useContext)
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,7 +22,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-<<<<<<< HEAD
     <AuthContextProvider>
       <html lang="en">
         <body className={inter.className}>
@@ -37,15 +32,5 @@ export default function RootLayout({
         </body>
       </html>
     </AuthContextProvider>
-=======
-    <html lang="en">
-      <body className={inter.className}>
-        {/* <HeaderTop/> */}
-        <Header/>
-        {children}
-        <Footer />
-      </body>
-    </html>
->>>>>>> parent of ac85760 (finished useContext)
   );
 }
