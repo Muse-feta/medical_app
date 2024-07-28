dfcbc430b186f0a1e229a69c37d547586d62447b
Muse-feta <fetamuse@gmail.com>
2024-07-28 22:50:23 +0300

    still working

--- medical_app/app/layout.tsx
+++ medical_app/app/layout.tsx
@@ -4,8 +4,9 @@ import "./globals.css";
 import "@/asset/css/app.min.css";
 import "@/asset/css/style.css";
 import "@/asset/css/fontawesome.min.css";
-import { Footer, Header, HeaderTop } from "@/components";
 import AuthContextProvider from "@/context/authContext/AuthContextProvider";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
