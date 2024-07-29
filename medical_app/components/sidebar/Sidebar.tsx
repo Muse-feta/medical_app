0783e0f188196c8d2ccab19d2727c824389cd010
Muse-feta <fetamuse@gmail.com>
2024-07-30 01:13:23 +0300

    fix hydration issue

--- medical_app/components/sidebar/Sidebar.tsx
+++ medical_app/components/sidebar/Sidebar.tsx
@@ -1,6 +1,11 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Nav } from '../ui/Nav'
+import {
+  useWindowSize,
+  useWindowWidth,
+  useWindowHeight,
+} from "@react-hook/window-size";
 
 import { Button } from '../ui/button';
 import { AppWindow, ChevronRight, LayoutDashboard, Users } from 'lucide-react';
@@ -8,19 +13,27 @@ import { AppWindow, ChevronRight, LayoutDashboard, Users } from 'lucide-react';
 type Props = {}
 
 const Sidebar = (props: Props) => {
+  const [mobileWidth, setMobileWidth] = useState(false);
+  const onlyWidth = useWindowWidth();
     const [isCollapsed, setIsCollapsed] = useState(false)
+
+      useEffect(() => {
+        setMobileWidth(onlyWidth < 768);
+      }, [onlyWidth]);
   return (
     <div className="relative min-w-[80px] border-r px-4 pb-10 pt-[70px]">
       <div
         className="absolute top-0 right-[-20px] rounded-full my-3"
         onClick={() => setIsCollapsed(!isCollapsed)}
       >
-        <Button variant={"outline"} className="px-2 rounded-full">
-          <ChevronRight />
-        </Button>
+        {!mobileWidth &&(
+          <Button variant={"outline"} className="px-2 rounded-full">
+            <ChevronRight />
+          </Button>
+        )}
       </div>
       <Nav
-        isCollapsed={isCollapsed}
+        isCollapsed={mobileWidth ? true : isCollapsed}
         links={[
           {
             title: "Dashboard",
@@ -43,7 +56,6 @@ const Sidebar = (props: Props) => {
             icon: AppWindow,
             variant: "ghost",
           },
-          
         ]}
       />
     </div>
