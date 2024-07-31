bd0deffef697fac1c2e6c15a111d584816954834
Muse-feta <fetamuse@gmail.com>
2024-08-01 00:00:46 +0300

    finished dashbored client side

--- medical_app/app/(Routes)/admin/dashboard/appointments/page.tsx
+++ medical_app/app/(Routes)/admin/dashboard/appointments/page.tsx
@@ -1,11 +1,144 @@
-import React from 'react'
+"use client";
+import DashboardTitle from "@/components/ui/dashboardTitle";
+import { DataTable } from "@/components/ui/DataTable";
+import { cn } from "@/lib/utils";
+import { ColumnDef } from "@tanstack/react-table";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
-const DashboardAppointements = (props: Props) => {
+const DashboardOrders = (props: Props) => {
   return (
-    <div>DashboardAppointements</div>
-  )
-}
+    <div className="flex flex-col gap-5 w-full">
+      <DashboardTitle title="Appointments" />
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+};
 
-export default DashboardAppointements
+export default DashboardOrders;
+
+type Payment = {
+  order: number;
+  name: string;
+  phone: string;
+  status: "pending" | "processing" | "success" | "failed";
+  date: string;
+};
+
+export const data: Payment[] = [
+  {
+    order: 3,
+    name: "Muse",
+    phone: "fetamuse@gmail.com",
+    status: "pending",
+    date: "02/14/2024",
+  },
+  {
+    order: 3,
+    name: "Muse",
+    phone: "fetamuse@gmail.com",
+    status: "pending",
+    date: "02/14/2024",
+  },
+  {
+    order: 3,
+    name: "Muse",
+    phone: "fetamuse@gmail.com",
+    status: "pending",
+    date: "02/14/2024",
+  },
+  {
+    order: 3,
+    name: "Muse",
+    phone: "fetamuse@gmail.com",
+    status: "pending",
+    date: "02/14/2024",
+  },
+  {
+    order: 3,
+    name: "Muse",
+    phone: "fetamuse@gmail.com",
+    status: "pending",
+    date: "02/14/2024",
+  },
+  {
+    order: 3,
+    name: "Muse",
+    phone: "fetamuse@gmail.com",
+    status: "pending",
+    date: "02/14/2024",
+  },
+  {
+    order: 3,
+    name: "Muse",
+    phone: "fetamuse@gmail.com",
+    status: "pending",
+    date: "02/14/2024",
+  },
+  {
+    order: 3,
+    name: "Muse",
+    phone: "fetamuse@gmail.com",
+    status: "pending",
+    date: "02/14/2024",
+  },
+  {
+    order: 3,
+    name: "Muse",
+    phone: "fetamuse@gmail.com",
+    status: "pending",
+    date: "02/14/2024",
+  },
+  {
+    order: 3,
+    name: "Muse",
+    phone: "fetamuse@gmail.com",
+    status: "pending",
+    date: "02/14/2024",
+  },
+  {
+    order: 3,
+    name: "Muse",
+    phone: "fetamuse@gmail.com",
+    status: "pending",
+    date: "02/14/2024",
+  },
+ 
+  // ...
+];
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "order",
+    header: "Order",
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "phone",
+    header: "Phone",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      return (
+       <div className={cn('font-medium w-fit px-4 py-2 rounded-lg',
+       { "bg-red-200": row.getValue("status") === "pending",
+        "bg-orange-200": row.getValue("status") === "pending",
+        "bg-green-200": row.getValue("status") === "completed",
+      }
+       )}>
+        {row.getValue("status")}
+       </div>
+      );
+    },
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+  },
+];
--- medical_app/app/(Routes)/admin/dashboard/users/page.tsx
+++ medical_app/app/(Routes)/admin/dashboard/users/page.tsx
@@ -1,11 +1,146 @@
+"use client"
+import DashboardTitle from '@/components/ui/dashboardTitle';
+import { DataTable } from '@/components/ui/DataTable';
+import { ColumnDef } from '@tanstack/react-table';
 import React from 'react'
 
 type Props = {}
 
 const DashboardUsers = (props: Props) => {
   return (
-    <div>DashboardUsers</div>
-  )
+    <div className="flex flex-col gap-5 w-full">
+      <DashboardTitle title="Users" />
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
 
 export default DashboardUsers
+
+type Payment = {
+  name: string;
+  email: string;
+  status: "pending" | "processing" | "success" | "failed";
+  role: string;
+};
+
+export const data: Payment[] = [
+  {
+    name: "Muse",
+    email: "fetamuse@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  {
+    name: "Feta",
+    email: "feta@gmail.com",
+    status: "pending",
+    role: "admin",
+  },
+  // ...
+];
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ row }) => {
+      return (
+        <div className="flex items-center gap-2">
+          <img
+            className="w-4 h-4 rounded-full"
+            src={`https://api.dicebear.com/9.x/lorelei/svg?seed=${row.getValue(
+              "name"
+            )}`}
+            alt="avater"
+          />
+          <span>{row.getValue("name")}</span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "role",
+    header: "Role",
+  },
+];
